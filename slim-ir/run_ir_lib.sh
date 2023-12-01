3637aac291c306bd9107d224c3174e2ff3f89533
jbezoz <jbezoz007@gmail.com>
2023-12-01 06:12:16 +0530

    bug fix 3

--- slim-ir/run_ir_lib.sh
+++ slim-ir/run_ir_lib.sh
@@ -3,11 +3,10 @@ if [ -d "build" ]; then
     # If it exists, remove its contents
     sudo rm -r build/*
 else
-    # If it doesn't exist, print a message or take alternative actions
-    echo "The 'build' directory does not exist. Please create it before running this script."
-    # You can add more commands or exit the script if needed
+    # If it doesn't exist, make a dir name build
+    echo "Creating build"
 fi
-bash ./clean_ir_lib.sh
-mkdir -p build && cd build
+mkdir build 
+cd build
 cmake -S .. -B .
 sudo make install
--- slim-use/run.sh
+++ slim-use/run.sh
@@ -2,10 +2,10 @@ if [ -d "build" ]; then
     # If it exists, remove its contents
     rm -r build/*
 else
-    # If it doesn't exist, print a message or take alternative actions
-    echo "The 'build' directory does not exist. Please create it before running this script."
-    # You can add more commands or exit the script if needed
+    # If it doesn't exist, make a dir name build
+    echo "Creating build"
 fi
+mkdir build/
 cd build/ && cmake .. 
 make
 ./cs-ssa ../testcases/test.slim.ll

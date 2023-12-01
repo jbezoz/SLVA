c80e7ea78713c05454efac220dae352eec1ead6f
jbezoz <jbezoz007@gmail.com>
2023-12-01 05:54:31 +0530

    bug fix 2

--- slim-ir/run_ir_lib.sh
+++ slim-ir/run_ir_lib.sh
@@ -1,4 +1,12 @@
-sudo rm -fr build/
+# sudo rm -fr build/
+if [ -d "build" ]; then
+    # If it exists, remove its contents
+    sudo rm -r build/*
+else
+    # If it doesn't exist, print a message or take alternative actions
+    echo "The 'build' directory does not exist. Please create it before running this script."
+    # You can add more commands or exit the script if needed
+fi
 bash ./clean_ir_lib.sh
 mkdir -p build && cd build
 cmake -S .. -B .

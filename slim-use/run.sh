cd26adba421971b53c567310841addcd61d56ca0
jbezoz <jbezoz007@gmail.com>
2023-12-01 05:46:55 +0530

    corrected bug

--- slim-use/run.sh
+++ slim-use/run.sh
@@ -1,4 +1,11 @@
-rm -r build/*
+if [ -d "build" ]; then
+    # If it exists, remove its contents
+    rm -r build/*
+else
+    # If it doesn't exist, print a message or take alternative actions
+    echo "The 'build' directory does not exist. Please create it before running this script."
+    # You can add more commands or exit the script if needed
+fi
 cd build/ && cmake .. 
 make
 ./cs-ssa ../testcases/test.slim.ll
